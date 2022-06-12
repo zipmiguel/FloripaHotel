17a685447ab99e224ed20f2752c29251e7522378
luiz eduardo sagaz carvalho <masterdudu123@hotmail.com>
2022-06-12 04:59:48 -0300

    popup de editar pronto realmente git add .

--- hotel/src/main/resources/static/js/tabela.js
+++ hotel/src/main/resources/static/js/tabela.js
@@ -1,28 +1,46 @@
+// feito por luiz eduardo sagaz carvalho
+
+
 function mostrarPopup(){
     const listaOpen = Array.from(document.querySelectorAll('.botaoPopup'))
     listaOpen.forEach(open => {
         document.querySelector('.popup').classList.add('show');
     }) 
 }
-// o parametro id é o ID do <a id="">
 function ocultarPopup(){
    const listaClose = Array.from(document.querySelectorAll('.close'));
     listaClose.forEach(close => {
         document.querySelector('.popup').classList.remove('show');
     }) 
 }
-function editarPopup(){
-    mostrarPopup();
-    const listaEditar = Array.from(document.querySelectorAll('.editarPopup'));
-    listaEditar.forEach(tt => function(){
-        document.querySelector('.')
-    })
+function OpenEditPopup(idquarto){
+       const popupEditar = document.getElementById('editarPopup');
+       popupEditar.removeAttribute('idquarto');
+       popupEditar.setAttribute('idquarto',idquarto);
+       mostrarPopup()
+}
+function CloseEditPopup(){
+    const listaDiv = Array.from(document.querySelectorAll('#editarPopup div'));
+    for (let i = 0; i < listaDiv.length; i++) {
+        const divAtual = listaDiv[i].children[1];
+        console.log(divAtual.tagName);
+        switch (divAtual.tagName) {
+            case 'INPUT':
+                divAtual.value = ""; 
+            break;
+                
+            case 'SELECT':
+                const tipoSelect = divAtual.id;
+                document.getElementById(tipoSelect).value = divAtual.children[0].value;
+                break;
+            }
+    }
+    ocultarPopup();
 }
 function gerarQNTpessoas(){
     const camaSolteiro = parseInt($("#quantidadeCamaSolteiro").val());
     const camaCasal = parseInt($("#quantidadeCamaCasal").val());
     document.getElementById('numeroPessoas').value = camaSolteiro+camaCasal;
 }
-
 
 
