d54aa821b4124bd93aff0927b41e5125576fd539
Eduardo g de lazari <90356149+edugdl@users.noreply.github.com>
2022-06-13 21:55:21 -0300

    passando lista para mysql em formato de json

---
+++ hotel/src/main/java/br/com/hotel/repositorio/QuartoRepositorio.java
@@ -0,0 +1,11 @@
+package br.com.hotel.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.hotel.model.Quarto;
+
+@Repository
+public interface QuartoRepositorio extends JpaRepository<Quarto, Long>{
+
+}
