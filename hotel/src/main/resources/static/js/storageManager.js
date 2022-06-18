a0f12a93f641ef5cbc64690234d580018c1f487e
luiz eduardo sagaz carvalho <masterdudu123@hotmail.com>
2022-06-17 22:30:57 -0300

    hotfix

--- hotel/src/main/resources/static/js/storageManager.js
+++ hotel/src/main/resources/static/js/storageManager.js
@@ -9,7 +9,7 @@ function checkUser(){
     try {
     if (localStorage.getItem('user') != null || localStorage.getItem('user') != undefined) {
         const user = JSON.parse(localStorage.getItem("user"))
-        divLoginLogout.innerHTML = `<h3 class="negrito branco">${user.nome}/h3> <button class="pointer" onclick="Logout()" id="logout" >Logout</button>`
+        divLoginLogout.innerHTML = `<h3 class="negrito branco"> ${(user.nome).toUpperCase()} </h3> <button class="pointer" onclick="Logout()" id="logout" >Logout</button>`
     }else{
         divLoginLogout.innerHTML = `<div class="nav-right"> <a href="./index.html#reserva" class="primary-btn">Faça sua Reserva</a> <a href="./loginUsuario.html" id="button-login">Login</a></div>`
     }
