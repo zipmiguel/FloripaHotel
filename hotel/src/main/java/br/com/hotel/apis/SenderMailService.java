1906975c34c8b75ed96fb90b1b7ac330447c769e
Eduardo g de lazari <eduardogdelazari2005@gmail.com>
2022-06-24 01:10:43 -0300

    verificacao de form, enviar email async, recuperarsenha funcionando

--- hotel/src/main/java/br/com/hotel/apis/SenderMailService.java
+++ hotel/src/main/java/br/com/hotel/apis/SenderMailService.java
@@ -8,6 +8,7 @@ import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,11 +16,8 @@ public class SenderMailService {
 
     @Autowired
     private JavaMailSender mailSender;
-    public String enviarRecuperarSenha(String endereco, String nome) throws MessagingException {
-        MimeMessage mimeMessage= mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
-        helper.setTo(endereco);
-        helper.setSubject("Recuperação de Senha");
+    
+    public String codigoRecuperarSenha(String endereco){
         endereco = endereco.replace(".", "").replace("@","");
         int tamanho = endereco.length()-1;
         Random gerador = new Random();
@@ -35,19 +33,30 @@ public class SenderMailService {
             }
         }
         codigo = codigo.toUpperCase();
-        helper.setText("Insira esse código para alterar sua senha: "+codigo);
+        return codigo;
+    }
 
+    @Async
+    public void enviarRecuperarSenha(String endereco, String nome, String codigo) throws MessagingException {
+        MimeMessage mimeMessage= mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+        helper.setTo(endereco);
+        helper.setSubject("Recuperação de Senha");
+        helper.setText("Insira esse código para alterar sua senha: "+codigo);
         helper.setText("<html><head></head><body><h2 style=\"color: black;\">Floripa Hotel</h2> <br/>"+
         "<h3 style=\"color: black;\">Olá "+nome+" !<br/><br/>"+
         "<h2>"+codigo+"</h2><br/>"+
         "<h3> <a href=\"http://localhost:8089/recuperarSenhaUsuario\">Clique aqui para ir para a página de alteração de senha e insira o código acima !</a></h3></body></html>", true);
         mailSender.send(mimeMessage);
-        
-        return codigo;
+
     }
-    
-    public int enviarConfirmarCadastro(String endereco, String nome) throws MessagingException {
-        int codigo = (int)System.currentTimeMillis();
+
+    public int codigoFinalizarCadastro(){
+        return (int)System.currentTimeMillis();
+    }
+
+    @Async
+    public void enviarConfirmarCadastro(String endereco, String nome, int codigo) throws MessagingException {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
         helper.setTo(endereco);
@@ -56,6 +65,5 @@ public class SenderMailService {
         "<h3 style=\"color: black;\">Olá "+nome+", seu cadastro está quase concluído, por favor clique no link abaixo para finalizá-lo !<br/><br/>"+
         "<a href=\"http://localhost:8089/finalizarCadastro/"+codigo+"\">Clique aqui!</a></h3></body></html>", true);
         mailSender.send(mimeMessage);
-        return codigo;
     }
 }
--- hotel/src/main/java/br/com/hotel/controller/HospedeController.java
+++ hotel/src/main/java/br/com/hotel/controller/HospedeController.java
@@ -6,6 +6,7 @@ import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import br.com.hotel.model.Hospede;
 import br.com.hotel.repositorio.HospedeRepositorio;
 
 @RestController
+@EnableAsync
 public class HospedeController {
 
     @Autowired
@@ -27,60 +29,82 @@ public class HospedeController {
 
     @PostMapping("/cadastrarHospede")
     public void adicionar(Hospede hospede, HttpServletResponse response) throws IOException, MessagingException {
-        int codigo = senderMailService.enviarConfirmarCadastro(hospede.getEmail(), hospede.getNome());
+        int codigo = senderMailService.codigoFinalizarCadastro(); 
+        senderMailService.enviarConfirmarCadastro(hospede.getEmail(), hospede.getNome(), codigo);
         hospede.setVerificarConfirmacao(codigo);
         hospedeRepositorio.save(hospede);
         response.sendRedirect("/loginUsuario");
     }
 
     @GetMapping("/finalizarCadastro/{codigo}")
-    public void finalizarCadastro(@PathVariable("codigo") int codigo) {
+    public void finalizarCadastro(HttpServletResponse response, @PathVariable("codigo") int codigo) throws IOException {
         Hospede hospede = hospedeRepositorio.findByverificarConfirmacao(codigo);
         hospede.setVerificarConfirmacao(-1);
         hospedeRepositorio.save(hospede);
+        response.sendRedirect("/loginUsuario");
     }
 
     @PostMapping("/editarHospede")
     public Hospede editarHospede(Hospede hospede, HttpServletResponse response) throws CloneNotSupportedException, IOException {
-        Hospede hospedeExistente = hospedeRepositorio.findBycpf(hospede.getCpf());
-        Long id = hospedeExistente.getIdHospede();
-        hospedeExistente = hospede.clone();
-        hospedeExistente.setIdHospede(id);
-        hospedeExistente.setVerificarConfirmacao(-1);
-        hospedeRepositorio.save(hospedeExistente);
-        return hospedeExistente;
-    }
-
-    @GetMapping("/verificarLogin/hospede")
-    public Hospede verificarLoginHospede(HttpServletResponse response, @RequestParam String login, @RequestParam String senha){
-        Hospede hospede = hospedeRepositorio.findByemail(login);
-        if(hospede.getSenha().equals(senha) && hospede.getVerificarConfirmacao() == -1){
-            return hospede;
-        }else if(hospede.getSenha().equals(senha)){
-            Hospede hospede2 = new Hospede();
-            hospede2.setNome("sayudasd@#aidshuauisg86aSDBH");
-            return hospede2; 
-        }
-        else{
+        try {
+            Hospede hospedeExistente = hospedeRepositorio.findBycpf(hospede.getCpf());
+            Long id = hospedeExistente.getIdHospede();
+            hospedeExistente = hospede.clone();
+            hospedeExistente.setIdHospede(id);
+            hospedeExistente.setVerificarConfirmacao(-1);
+            hospedeRepositorio.save(hospedeExistente);
+            return hospedeExistente;
+        } catch (Exception e) {
             return null;
         }
     }
 
-    @GetMapping("/recuperarSenha")
-    public void enviarRecuperacaoSenha(HttpServletResponse response, @RequestParam(name = "email") String email)
+    @PostMapping("/verificarLogin/hospede")
+    public Hospede verificarLoginHospede(HttpServletResponse response, @RequestParam String login, @RequestParam String senha){
+        try {
+            Hospede hospede = hospedeRepositorio.findByemail(login);
+            if(hospede.getSenha().equals(senha) && hospede.getVerificarConfirmacao() == -1){
+                return hospede;
+            }else if(hospede.getSenha().equals(senha)){
+                Hospede hospede2 = new Hospede();
+                hospede2.setNome("sayudasd@#aidshuauisg86aSDBH");
+                return hospede2; 
+            }
+            else{
+                return null;
+            }
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @PostMapping("/recuperarSenha")
+    public Hospede enviarRecuperacaoSenha(HttpServletResponse response, @RequestParam(name = "email") String email)
             throws MessagingException, IOException {
-        Hospede hospede = hospedeRepositorio.findByemail(email);
-        String codigo = senderMailService.enviarRecuperarSenha(email,hospede.getEmail());
-        hospede.setSenha(codigo);
-        hospedeRepositorio.save(hospede);
-        response.sendRedirect("/recuperarSenhaUsuario");
+        try {
+            Hospede hospede = hospedeRepositorio.findByemail(email);
+            String codigo = senderMailService.codigoRecuperarSenha(email);
+            senderMailService.enviarRecuperarSenha(email,hospede.getEmail(),codigo);
+            hospede.setSenha(codigo);
+            hospedeRepositorio.save(hospede);
+            response.sendRedirect("/recuperarSenhaUsuario");
+            return hospede;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
     }
 
     @PostMapping("/trocarSenha")
-    public void recuperarSenha(HttpServletResponse response, @RequestParam(name = "novaSenha") String novaSenha, @RequestParam(name = "codigo") String codigo){
-        Hospede hospede = hospedeRepositorio.findBysenha(codigo);
-        hospede.setSenha(novaSenha);
-        hospedeRepositorio.save(hospede);
+    public Boolean recuperarSenha(HttpServletResponse response, @RequestParam(name = "novaSenha") String novaSenha, @RequestParam(name = "codigo") String codigo){
+        try {
+            Hospede hospede = hospedeRepositorio.findBysenha(codigo);
+            hospede.setSenha(novaSenha);
+            hospedeRepositorio.save(hospede);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
 }
